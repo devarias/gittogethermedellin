06042a6f6c90230c491025f7157e086a8c57d1c2
David Arias Fuentes <61300552+devarias@users.noreply.github.com>
2025-02-20 20:06:35 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-0c55b159cbfafe1f0" # AMI Amazon Linux 2
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "Servidor-Terraform"
+  }
+}
